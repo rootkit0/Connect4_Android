de31522e70b43c7c9200f6c0bf6159c6e927f2e7
Xavier <xaavier@protonmail.com>
2020-04-03 22:20:12 +0200

    Added help layout and functionality to help and exit buttons

--- app/src/main/java/com/example/connect4/AjudaActivity.java
+++ app/src/main/java/com/example/connect4/AjudaActivity.java
@@ -1,6 +1,9 @@
 package com.example.connect4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,18 @@ public class AjudaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajuda);
+
+        Button btn_goBack = (Button)findViewById(R.id.button3);
+        btn_goBack.setOnClickListener(new goBack());
+    }
+
+    private class goBack implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            setResult(RESULT_OK, intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/connect4/MainActivity.java
+++ app/src/main/java/com/example/connect4/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.connect4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_startGame = (Button)findViewById(R.id.button);
+        btn_startGame.setOnClickListener(new startGame());
+
+        Button btn_gameRules = (Button)findViewById(R.id.button1);
+        btn_gameRules.setOnClickListener(new gameRules());
+
+        Button btn_exitGame = (Button)findViewById(R.id.button2);
+        btn_exitGame.setOnClickListener(new exitGame());
+    }
+
+    private class startGame implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private class gameRules implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            //Create intent to run second activity
+            Intent i = new Intent(MainActivity.this, AjudaActivity.class);
+            startActivityForResult(i, 1);
+        }
+    }
+
+    private class exitGame implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            System.exit(0);
+        }
     }
 }
